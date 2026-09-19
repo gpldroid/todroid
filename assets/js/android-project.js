21fefd32fad61045556729b9c0893345b9881c33
imad limrani <148809071+gpldroid@users.noreply.github.com>
2026-09-19 18:52:00 +0100

    refine: request device permissions only in WebView context

--- assets/js/android-project.js
+++ assets/js/android-project.js
@@ -298,7 +298,9 @@ public class MainActivity extends AppCompatActivity {
   }
 
   function settingsGradle(c) {
-    return `pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
+    return `import org.gradle.api.initialization.resolve.RepositoriesMode
+
+pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories { google(); mavenCentral() }
