f264043233c08c62cae6ef51a9a42347e196bff0
imad limrani <148809071+gpldroid@users.noreply.github.com>
2026-09-19 17:46:38 +0100

    refactor: add assets/js/tailwind.config.js

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,25 @@
+tailwind.config = {
+            darkMode: 'class',
+            theme: {
+                extend: {
+                    colors: {
+                        brand: {
+                            50: '#eef2ff',
+                            100: '#e0e7ff',
+                            400: '#818cf8',
+                            500: '#6366f1',
+                            600: '#4f46e5',
+                            700: '#4338ca',
+                            900: '#312e81',
+                        },
+                        darkbg: '#080a11',
+                        darkcard: '#101524',
+                        darkborder: '#1d263b'
+                    },
+                    fontFamily: {
+                        sans: ['Inter', 'sans-serif'],
+                        mono: ['Fira Code', 'JetBrains Mono', 'monospace']
+                    }
+                }
+            }
+        }
